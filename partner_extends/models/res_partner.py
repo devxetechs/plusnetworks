1bdb487a8e13cec89beaad2643708aaf23e7aed8
jquintero-xt <jquintero@xetechs.com>
2023-02-13 10:10:07 -0400

    refix: translate and fields

--- partner_extends/models/res_partner.py
+++ partner_extends/models/res_partner.py
@@ -12,8 +12,9 @@ class ResPartner(models.Model):
 
     zone = fields.Char(string="Zone")
     municipality_id = fields.Many2one("res.country.municipality", string="Municipality")
+    speed = fields.Char(string="Bandwidth")
+    ip_assigned = fields.Char(string="Assigned Public IP")
 
-    speed = fields.Char(string="Speed")
     link_code = fields.Char(string="Link Code")
     annex_code = fields.Char(string="Annex code")
     att_expiration_date = fields.Date(string='Attachment Expiration Date', store=True, default=fields.Datetime.now)
