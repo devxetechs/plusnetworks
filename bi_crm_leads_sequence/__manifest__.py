a90d4fecd82abdd4377b25d88a3e460218dc28de
jquintero-xt <jquintero@xetechs.com>
2023-06-15 14:21:45 -0400

    add: modules

---
+++ bi_crm_leads_sequence/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Auto Sequance on lead Form',
+    'version': '16.0.0.2',
+    'category': 'Extra Tools',
+    'license': 'OPL-1',
+    'summary': 'This apps helps create/generate automatic sequence of lead',
+    'description':"""Automatic Sequance on Lead, Lead Sequance, Auto numbering on lead ,Automatic Sequance on oppertunity, oppertunity Sequance, Auto numbering on oppertunity, Automatic Sequance on pipeline, pipeline Sequance, Auto numbering on pipline,""", 
+    'author': 'BrowseInfo',
+    'website': 'https://www.browseinfo.in',
+    'depends':['base','sale','crm'],
+    'data':[
+        'data/ir_sequence_data.xml',
+        'views/bi_lead.xml',
+        ],
+    'installable': True,
+    'auto_install': False,
+    "live_test_url":'https://youtu.be/e3ZtVCOuI3E',
+    "images":['static/description/Banner.gif'],
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
