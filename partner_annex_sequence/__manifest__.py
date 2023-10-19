eec73b96e1095a92ab5620075ee4da3fb86bd80f
ofloresxetechs <oflores@exetechs.com>
2023-10-19 15:31:32 -0400

    nuevo modulo secuencia de codigo anexo en contactos

---
+++ partner_annex_sequence/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'partner annex sequence',
+    'version': '15.0',
+    'category': '',
+    'description': """ partner annex sequence """,
+    'author': 'Omar Flores - oflores@xetechs.com',
+    'website': 'https://www.xetechs.com',
+    'depends': ['base', 'partner_extends'],
+    'data': [
+        'data/partner_data.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'active': False,
+    'license': 'LGPL-3',
+
+}
